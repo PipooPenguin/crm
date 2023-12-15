63ed72e2194e026cc354bacf252d3db44d720f49
DuongHuyHoang <henryhoangduong@gmail.com>
2023-12-15 17:39:01 +0700

    commit

--- src/component/Ladipage.jsx
+++ src/component/Ladipage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import axios from "axios";
-import ThankYou from "./ThankYou";
+import ThankYou from "./Thanks";
 
 
 const Ladipage = () => {
